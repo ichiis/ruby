7467bfa21a565dfc324a8248cee2781fd87c52ae
EC2 Default User <ec2-user@ip-172-31-35-78.ap-northeast-1.compute.internal>
2023-06-05 17:38:13 +0900

    [ruby]new3

--- new.rb
+++ new.rb
@@ -1,4 +1,9 @@
 class Car 
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
@@ -9,8 +14,8 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
-car.run(5)
+car.move("右", 5)
+
 
 class Car
   def self.run(distance)
